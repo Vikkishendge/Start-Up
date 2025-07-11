b2649553608d604c2cd7b57661706b882ee9a319
vikas <vikasshendge1202@gmail.com>
2025-07-12 02:55:19 +0300

    orm connection

--- my-app/server.js
+++ my-app/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./config/db');
 const cors = require('cors');
 require('dotenv').config();
 const userRoutes = require('./routes/user.route');
@@ -11,3 +12,16 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+sequelize.sync()
+  .then(() => {
+    console.log('✅ Database synced');
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('❌ Failed to sync DB:', err);
+  });
